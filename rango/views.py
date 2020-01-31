451d49a1e8f01da08a83f52a3d65d66893bf9e6a
Marty O'Neill <2335418o@student.gla.ac.uk>
2020-01-31 17:30:18 +0000

    Chapter 6 complete, all tests passed'

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    # Contstruct a dictionary to pass to the template engine as its context
-    # 'boldmessage' matches the variable {{boldmessage}} in template
-    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+from rango.models import Category
+from rango.models import Page
 
-    # returns a rendered response ro sedn to the client
-    # make sure of the shortcut funtion (? what is this)
-    # first param is template we wish to use
+def index(request):
+
+    '''
+    > Query the database for a list of ALL categories curently stored
+    > Order the caegories by the number of likes in descnding order
+    > Retrieve the top 5 only -- or all if less than 5
+    > Place the list in our context_dict dictionary (with boldmessage)
+    > this will be passed to the template engine
+    '''
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
+    # Render response and send back
     return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
     context_dict = {}
     return render(request, 'rango/about.html', context = context_dict)
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+
+def show_page(request, page_name_slug):
+    
+    context_dict ={}
+
+    try:
+        page = Page.objects.get(slug=page_name_slug)
+        context_dict['page'] = page
+
+    except Page.DoesNotExist:
+        context_dict['page'] = None
+
+    return render(request, 'rango/page.html', context=context_dict)
+
+
