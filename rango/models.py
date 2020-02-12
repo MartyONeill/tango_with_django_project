b2a639f91db969e47ae992b5594a18dd3305fead
Marty O'Neill <2335418o@student.gla.ac.uk>
2020-02-12 10:05:08 +0000

    Almost finished 7, deleting database

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,10 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+
+    NAME_MAX_LENGTH = 128
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -23,7 +26,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    #slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
